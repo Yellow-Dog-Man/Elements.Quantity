acb28ad677beec3d79ad1bedd3836943ee306794
Redd <pr.redstone@gmail.com>
2025-09-23 13:47:54 -0700

    add consistant spacing

--- Elements.Quantity/Quantities/Basic/Distance.cs
+++ Elements.Quantity/Quantities/Basic/Distance.cs
@@ -97,15 +97,19 @@ namespace Elements.Quantity
         public static readonly Unit<Distance> Thou = new Unit<Distance>(0.0000254,
             new UnitGroup[] { UnitGroup.Imperial },
             new string[] { " th" }, new string[] { " thou" });
+            
         public static readonly Unit<Distance> Inch = new Unit<Distance>(0.0254,
             new UnitGroup[] { UnitGroup.Imperial },
             new string[] { " in", "\"" }, new string[] { " inches", " inch" });
+            
         public static readonly Unit<Distance> Foot = new Unit<Distance>(0.3048,
             new UnitGroup[] { UnitGroup.Imperial },
             new string[] { " ft", "'" }, new string[] { " feet" });
+            
         public static readonly Unit<Distance> Yard = new Unit<Distance>(0.9144,
             new UnitGroup[] { UnitGroup.Imperial },
             new string[] { " yd" }, new string[] { " yards", " yard" });
+            
         public static readonly Unit<Distance> Mile = new Unit<Distance>(1609.344,
             new UnitGroup[] { UnitGroup.Imperial },
             new string[] { " mi" }, new string[] { " miles", " mile" });
