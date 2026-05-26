ab0fcf9fc4677a08a78ec061a63bf7705d0b5bac
redd <pr.redstone@gmail.com>
2026-05-25 23:12:14 -0700

    add in rankine and some info about what it is and the other temperature scales. like how rankine is like uber rare to be used

--- Elements.Quantity/Quantities/Basic/Temperature.cs
+++ Elements.Quantity/Quantities/Basic/Temperature.cs
@@ -42,19 +42,30 @@ namespace Elements.Quantity
         // define actual units for the quantity (excluding SI units which are automatic)
         // Parameters:
 
+
+        //zero Kelven is the coldest anything can be, zero movement.
         public static readonly Unit<Temperature> Kelvin = new Unit<Temperature>(1,
             new UnitGroup[] { UnitGroup.Common },
             new string[] { " K" }, new string[] { " Kelvins", " Kelvin" });
 
+        //Celsius, the type that is best used for science in most cases
         public static readonly Unit<Temperature> Celsius = new UnitNonLinear<Temperature>(
             (K)=>(K-273.15), (C)=>(C+273.15),
             new UnitGroup[] { UnitGroup.Common },
             new string[] { " °C" }, new string[] { " degrees Celsius", " degree Celsius", " Celsius" });
 
+
+        //Fahrenheit is the stupid temperature system for most things except how hot it feels outside.
         public static readonly Unit<Temperature> Fahrenheit = new UnitNonLinear<Temperature>(
             (K) => (K * (9.0 / 5.0) - 459.67), (F) => ((F + 459.67)*(5.0 / 9.0)),
             new UnitGroup[] { UnitGroup.Common },
             new string[] { " °F" }, new string[] { " degrees Fahrenheit", " degree Fahrenheit", " Fahrenheit" });
+
+        //Rankine is to fahrenheit as kelven is to celsius, its like rarely used but im putting it here for the sake of completion
+        public static readonly Unit<Temperature> Rankine = new UnitNonLinear<Temperature>(
+            (K) => (K * (9.0 / 5.0)), (R) => (R * (5.0 / 9.0)),
+            new UnitGroup[] { UnitGroup.Common },
+            new string[] { " °R" }, new string[] { " degrees Rankine", " degree Rankine", " Rankine" });
 
         #endregion
 
